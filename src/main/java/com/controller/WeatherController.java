c9b8044e2415964d03651cda1ed2144a0a644929
Kalyan9912 <relangimavayya123@gmail.com>
2026-05-21 12:07:12 +0530

    Update WeatherController.java

--- src/main/java/com/controller/WeatherController.java
+++ src/main/java/com/controller/WeatherController.java
@@ -1,1 +1,23 @@
+package com.weatherapp.controller;
 
+import com.weatherapp.service.WeatherService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/weather")
+@CrossOrigin("*")
+public class WeatherController {
+
+    private final WeatherService weatherService;
+
+    public WeatherController(WeatherService weatherService) {
+        this.weatherService = weatherService;
+    }
+
+    @GetMapping("/{city}")
+    public String getWeather(
+            @PathVariable String city) {
+
+        return weatherService.getWeather(city);
+    }
+}
