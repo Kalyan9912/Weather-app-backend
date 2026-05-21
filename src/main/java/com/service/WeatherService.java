3f9bfbd1bd3ed444ef22abf52cb86f9aff61cf17
Kalyan9912 <relangimavayya123@gmail.com>
2026-05-21 12:07:59 +0530

    Update WeatherService.java

--- src/main/java/com/service/WeatherService.java
+++ src/main/java/com/service/WeatherService.java
@@ -1,1 +1,29 @@
+package com.weatherapp.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class WeatherService {
+
+    private final String API_KEY =
+            "YOUR_OPENWEATHER_API_KEY";
+
+    public String getWeather(String city) {
+
+        String url =
+                "https://api.openweathermap.org/data/2.5/weather?q="
+                        + city
+                        + "&appid="
+                        + API_KEY
+                        + "&units=metric";
+
+        RestTemplate restTemplate =
+                new RestTemplate();
+
+        return restTemplate.getForObject(
+                url,
+                String.class
+        );
+    }
+}
